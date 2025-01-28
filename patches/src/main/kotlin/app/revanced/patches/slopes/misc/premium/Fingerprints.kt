4e887717b0ca0072ad696e0f07ebdc7d262b6226
Marek Veselý <notmarek1337@gmail.com>
2025-01-28 10:04:33 +0100

    Fix slopes fingerprint

--- patches/src/main/kotlin/app/revanced/patches/slopes/misc/premium/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/slopes/misc/premium/Fingerprints.kt
@@ -5,16 +5,16 @@ import com.android.tools.smali.dexlib2.AccessFlags
 
 internal val getCurrentSubscriptionType = fingerprint {
     accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL)
-    returns("Lcom/consumedbycode/slopes/vo/SubscriptionOrigin")
-    custom { method, classDef ->
-        classDef.endsWith("AccessController;") && method.name == "getCurrentSubscriptionType"
+    // returns("Lcom/consumedbycode/slopes/vo/SubscriptionOrigin")
+    custom { method, _ ->
+        method.name == "getCurrentSubscriptionType"
     }
 }
 
 internal val getPassExpiration = fingerprint {
     accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL)
-    returns("Ljava/time/Instant")
-    custom { method, classDef ->
-        classDef.endsWith("AccessController;") && method.name == "getPassExpiration"
+    // returns("Ljava/time/Instant")
+    custom { method, _ ->
+        method.name == "getPassExpiration"
     }
 }
